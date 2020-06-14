47c5c0a497292b4ce07219078217f26300aa1793
Raunakk02 <raunakk728@gmail.com>
2020-06-14 12:58:24 +0530

    added responsiveness to chart_bar

--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -7,46 +7,55 @@ class ChartBar extends StatelessWidget {
   ChartBar(this.day, this.spendingAmount, this.spendingAmountPCT);
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          height: 20,
-          child: FittedBox(
-            child: Text(
-              '\$${spendingAmount.toStringAsFixed(0)}',
+    return LayoutBuilder(builder: (ctx, constraints) {
+      return Column(
+        children: [
+          Container(
+            height: constraints.maxHeight * 0.15,
+            child: FittedBox(
+              child: Text(
+                '\$${spendingAmount.toStringAsFixed(0)}',
+              ),
             ),
           ),
-        ),
-        SizedBox(
-          height: 4,
-        ),
-        Container(
-          height: 60,
-          width: 10,
-          decoration: BoxDecoration(
-            color: Colors.grey[100],
-            border: Border.all(color: Colors.grey, width: 2),
-            borderRadius: BorderRadius.circular(10),
+          SizedBox(
+            height: constraints.maxHeight * 0.05,
           ),
-          child: Stack(
-            children: [
-              FractionallySizedBox(
-                heightFactor: spendingAmountPCT ,
-                child: Container(
+          Container(
+            height: constraints.maxHeight * 0.6,
+            width: 10,
+            child: Stack(
+              children: [
+                Container(
                   decoration: BoxDecoration(
-                    color: Theme.of(context).primaryColor,
+                    color: Colors.grey[100],
+                    border: Border.all(color: Colors.grey, width: 2),
                     borderRadius: BorderRadius.circular(10),
                   ),
                 ),
-              )
-            ],
+                FractionallySizedBox(
+                  heightFactor: spendingAmountPCT,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).primaryColor,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
-        ),
-        SizedBox(
-          height: 4,
-        ),
-        Text(day),
-      ],
-    );
+          SizedBox(
+            height: constraints.maxHeight * 0.05,
+          ),
+          Container(
+            height: constraints.maxHeight * 0.15,
+            child: FittedBox(
+              child: Text(day),
+            ),
+          ),
+        ],
+      );
+    });
   }
 }
